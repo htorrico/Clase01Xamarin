6a224e55abbdfb040935018c51d54b6a883ff7d3
Hugo Torrico <htorrico@truenorthcorporation.com>
2020-11-19 21:08:40 -0500

    Agregar archivos de proyecto.

---
+++ Clase01Xamarin/Operaciones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase01Xamarin
+{
+    public class Operacion
+    {
+        //Atributos/Propieades
+        public int PrimerNumero { get; set; }
+        public int SegundoNumero { get; set; }
+        public int Resultado { get; set; }
+
+        //Funciones
+        public int Sumar(int a, int b) {
+
+            return a + b;
+        }
+
+        public string NombresCompletos(string nombre, 
+                                       string apellido)
+        {
+            return nombre + " " + apellido;
+        }
+
+        //Procedimientos
+        public void suma(int num1, int num2)
+        {
+            Resultado = num1 + num2;
+        }
+
+    }
+}
---
+++ Clase01Xamarin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase01Xamarin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declarar variables
+            //tipoVariable nombreVariable;
+            int Edad;
+            string Nombre;
+            int Nota = 18;
+            string Apellido = "Torrico";
+
+            //Instancias objetos
+            Operacion operacion = new Operacion();
+            operacion.PrimerNumero = 10; //operacion.setPrimerNumero(20);
+            operacion.SegundoNumero = 20;
+
+            Operacion operacion1 = new Operacion
+            {
+                PrimerNumero = 10,
+                SegundoNumero = 20
+            };
+            
+
+
+
+
+
+            Console.Read();
+        }
+
+        
+
+
+
+    }
+}
